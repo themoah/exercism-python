3342ca3dcdcd94a8c519272b026efd1cf3ca8b95
Aviv <bdozorets@gmail.com>
2017-08-02 01:04:45 +0300

    Word Count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,15 @@
-def word_count():
-    pass
+import collections
+
+def word_count(args):
+    convertArgsToString = str(args)
+    listOfChars = ''
+    for char in list(convertArgsToString):
+        if char.isalpha() or char.isdigit():
+            listOfChars += char.lower()
+        else:
+            listOfChars += ' '
+
+    listOfCharsWithOutSpaces = " ".join(listOfChars.split())
+    parsedArgs = listOfCharsWithOutSpaces.split(' ')
+    count = collections.Counter(parsedArgs)
+    return count
